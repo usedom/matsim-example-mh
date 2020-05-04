a2bc3b4b7005637fe291b96c194dfbc7a61bef6c
usedom <marcel.heine@vodafone.de>
2020-05-04 18:50:49 +0200

    "200427 first steps"

--- src/main/java/org/matsim/project/RunMatsim_for_exe.java
+++ src/main/java/org/matsim/project/RunMatsim_for_exe.java
@@ -18,19 +18,25 @@
  * *********************************************************************** */
 package org.matsim.project;
 
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.contrib.otfvis.OTFVisLiveModule;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.*;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.scenario.ScenarioUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author nagel
  *
  */
-public class RunMatsim {
+public class RunMatsim_for_exe {
 
 	public static void main(String[] args) {
 
@@ -47,6 +53,50 @@ public class RunMatsim {
 		// ---
 
 		Scenario scenario = ScenarioUtils.loadScenario(config);
+		Id<Person> interestingPersonId = Id.createPersonId(1);
+		List<Id<Person>> personsToRemove = new ArrayList<>();
+
+		for (Id<Person> personId : scenario.getPopulation().getPersons().keySet()) {
+			if (!personId.equals(interestingPersonId)) {
+				personsToRemove.add(personId);
+			}
+		}
+
+		for (Id<Person> personId : personsToRemove) {
+			scenario.getPopulation().removePerson(personId);
+		}
+
+		System.out.println("Population size = " + scenario.getPopulation().getPersons().size());
+
+		// We add one new agent with a simple plan
+		PopulationFactory populationFactory = scenario.getPopulation().getFactory();
+
+		Person person2 = populationFactory.createPerson(Id.createPersonId("Dominik"));
+
+		Plan plan = populationFactory.createPlan();
+
+		Activity homeActivity = populationFactory.createActivityFromLinkId("h", Id.createLinkId(21));
+		homeActivity.setEndTime(8*60*60.);
+		plan.addActivity(homeActivity);
+
+		Leg leg = populationFactory.createLeg(TransportMode.car);
+		plan.addLeg(leg);
+
+		Activity workActivity = populationFactory.createActivityFromLinkId("w", Id.createLinkId(1));
+		homeActivity.setEndTime(17*60*60.);
+		plan.addActivity(workActivity);
+
+		Leg leg2 = populationFactory.createLeg(TransportMode.car);
+		plan.addLeg(leg2);
+
+		Activity homeActivity2 = populationFactory.createActivityFromLinkId("h", Id.createLinkId(21));
+		plan.addActivity(homeActivity2);
+
+		person2.addPlan(plan);
+
+		scenario.getPopulation().addPerson(person2);
+
+		System.out.println("Population size = " + scenario.getPopulation().getPersons().size());
 
 		// possibly modify scenario here
 
